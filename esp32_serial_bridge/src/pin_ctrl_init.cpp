48206089e1b7eac368be37114f227239c6e7803d
ogawast19083 <minionkawaii-kgunso@docomo.ne.jp>
2026-02-14 23:36:07 +0900

    ENC2から4に増やしたい

--- esp32_serial_bridge/src/pin_ctrl_init.cpp
+++ esp32_serial_bridge/src/pin_ctrl_init.cpp
@@ -103,6 +103,11 @@ void ENCx2_SWx8_init() {
     gpio_set_pull_mode((gpio_num_t)ENC1_B, GPIO_PULLUP_ONLY);
     gpio_set_pull_mode((gpio_num_t)ENC2_A, GPIO_PULLUP_ONLY);
     gpio_set_pull_mode((gpio_num_t)ENC2_B, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode((gpio_num_t)ENC3_A, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode((gpio_num_t)ENC3_B, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode((gpio_num_t)ENC4_A, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode((gpio_num_t)ENC4_B, GPIO_PULLUP_ONLY);
+
 
     // パルスカウンタの設定
     pcnt_config_t pcnt_config1 = {};
@@ -153,28 +158,90 @@ void ENCx2_SWx8_init() {
     pcnt_config4.unit = PCNT_UNIT_1;
     pcnt_config4.channel = PCNT_CHANNEL_1;
 
+    pcnt_config_t pcnt_config5 = {};
+    pcnt_config1.pulse_gpio_num = ENC1_A;
+    pcnt_config1.ctrl_gpio_num = ENC1_B;
+    pcnt_config1.lctrl_mode = PCNT_MODE_KEEP;
+    pcnt_config1.hctrl_mode = PCNT_MODE_REVERSE;
+    pcnt_config1.pos_mode = PCNT_COUNT_INC;
+    pcnt_config1.neg_mode = PCNT_COUNT_DEC;
+    pcnt_config1.counter_h_lim = COUNTER_H_LIM;
+    pcnt_config1.counter_l_lim = COUNTER_L_LIM;
+    pcnt_config1.unit = PCNT_UNIT_2;
+    pcnt_config1.channel = PCNT_CHANNEL_0;
+
+    pcnt_config_t pcnt_config6 = {};
+    pcnt_config2.pulse_gpio_num = ENC1_B;
+    pcnt_config2.ctrl_gpio_num = ENC1_A;
+    pcnt_config2.lctrl_mode = PCNT_MODE_REVERSE;
+    pcnt_config2.hctrl_mode = PCNT_MODE_KEEP;
+    pcnt_config2.pos_mode = PCNT_COUNT_INC;
+    pcnt_config2.neg_mode = PCNT_COUNT_DEC;
+    pcnt_config2.counter_h_lim = COUNTER_H_LIM;
+    pcnt_config2.counter_l_lim = COUNTER_L_LIM;
+    pcnt_config2.unit = PCNT_UNIT_2;
+    pcnt_config2.channel = PCNT_CHANNEL_1;
+
+    pcnt_config_t pcnt_config7 = {};
+    pcnt_config3.pulse_gpio_num = ENC2_A;
+    pcnt_config3.ctrl_gpio_num = ENC2_B;
+    pcnt_config3.lctrl_mode = PCNT_MODE_KEEP;
+    pcnt_config3.hctrl_mode = PCNT_MODE_REVERSE;
+    pcnt_config3.pos_mode = PCNT_COUNT_INC;
+    pcnt_config3.neg_mode = PCNT_COUNT_DEC;
+    pcnt_config3.counter_h_lim = COUNTER_H_LIM;
+    pcnt_config3.counter_l_lim = COUNTER_L_LIM;
+    pcnt_config3.unit = PCNT_UNIT_3;
+    pcnt_config3.channel = PCNT_CHANNEL_0;
+
+    pcnt_config_t pcnt_config8 = {};
+    pcnt_config4.pulse_gpio_num = ENC2_B;
+    pcnt_config4.ctrl_gpio_num = ENC2_A;
+    pcnt_config4.lctrl_mode = PCNT_MODE_REVERSE;
+    pcnt_config4.hctrl_mode = PCNT_MODE_KEEP;
+    pcnt_config4.pos_mode = PCNT_COUNT_INC;
+    pcnt_config4.neg_mode = PCNT_COUNT_DEC;
+    pcnt_config4.counter_h_lim = COUNTER_H_LIM;
+    pcnt_config4.counter_l_lim = COUNTER_L_LIM;
+    pcnt_config4.unit = PCNT_UNIT_3;
+    pcnt_config4.channel = PCNT_CHANNEL_1;
+
     // パルスカウンタの初期化
     pcnt_unit_config(&pcnt_config1);
     pcnt_unit_config(&pcnt_config2);
     pcnt_unit_config(&pcnt_config3);
     pcnt_unit_config(&pcnt_config4);
+    pcnt_unit_config(&pcnt_config5);
+    pcnt_unit_config(&pcnt_config6);
+    pcnt_unit_config(&pcnt_config7);
+    pcnt_unit_config(&pcnt_config8);
 
     pcnt_counter_pause(PCNT_UNIT_0);
     pcnt_counter_pause(PCNT_UNIT_1);
+    pcnt_counter_pause(PCNT_UNIT_2);
+    pcnt_counter_pause(PCNT_UNIT_3);
 
     pcnt_counter_clear(PCNT_UNIT_0);
     pcnt_counter_clear(PCNT_UNIT_1);
+    pcnt_counter_clear(PCNT_UNIT_2);
+    pcnt_counter_clear(PCNT_UNIT_3);
 
     pcnt_counter_resume(PCNT_UNIT_0);
     pcnt_counter_resume(PCNT_UNIT_1);
+    pcnt_counter_resume(PCNT_UNIT_2);
+    pcnt_counter_resume(PCNT_UNIT_3);
 
     // チャタリング防止のフィルターを有効化
     pcnt_filter_enable(PCNT_UNIT_0);
     pcnt_filter_enable(PCNT_UNIT_1);
+    pcnt_filter_enable(PCNT_UNIT_2);
+    pcnt_filter_enable(PCNT_UNIT_3);
 
     // フィルター値を設定
     pcnt_set_filter_value(PCNT_UNIT_0, PCNT_FILTER_VALUE);
     pcnt_set_filter_value(PCNT_UNIT_1, PCNT_FILTER_VALUE);
+    pcnt_set_filter_value(PCNT_UNIT_2, PCNT_FILTER_VALUE);
+    pcnt_set_filter_value(PCNT_UNIT_3, PCNT_FILTER_VALUE);
 }
 
 void SERx4_TRx3_SWx4_init() {
